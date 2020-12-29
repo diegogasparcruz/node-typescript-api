8f33993a4503559f44b1e3fed7c50afb92eedceb
Diego Gaspar <diegogaspardacruz1@gmail.com>
2020-12-29 18:40:21 -0300

    Add error handling for StormGlass responses

--- src/clients/stormGlass.ts
+++ src/clients/stormGlass.ts
@@ -28,11 +28,17 @@ export interface ForecastPoint {
   windDirection: number;
   windSpeed: number;
 }
-
 export class ClientRequestError extends InternalError {
   constructor(message: string) {
     const internalMessage =
-      'Unexpected error when trying to communicate to StormGlass:';
+      'Unexpected error when trying to communicate to StormGlass';
+    super(`${internalMessage}: ${message}`);
+  }
+}
+export class StormGlassResponseError extends InternalError {
+  constructor(message: string) {
+    const internalMessage =
+      'Unexpected error returned by the StormGlass service';
     super(`${internalMessage}: ${message}`);
   }
 }
@@ -55,6 +61,13 @@ export class StormGlass {
       );
       return this.normalizeResponse(response.data);
     } catch (err) {
+      if (err.response && err.response.status) {
+        throw new StormGlassResponseError(
+          `Error: ${JSON.stringify(err.response.data)} Code: ${
+            err.response.status
+          }`
+        );
+      }
       throw new ClientRequestError(err.message);
     }
   }
