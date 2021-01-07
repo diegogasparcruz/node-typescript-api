fb780499a6ebb39c2011bfe295a20c7fd48313e8
Diego Gaspar <diegogaspardacruz1@gmail.com>
2021-01-07 01:17:29 -0300

    Corrects request in test users

--- test/functional/users.test.ts
+++ test/functional/users.test.ts
@@ -12,7 +12,7 @@ describe('Users functional tests', () => {
         password: '1234',
       };
 
-      const response = await global.testRequest.post('users').send(newUser);
+      const response = await global.testRequest.post('/users').send(newUser);
       expect(response.status).toBe(201);
       expect(response.body).toEqual(expect.objectContaining(newUser));
     });
