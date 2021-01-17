760ec3e581068255e36b8a3ccdb928cd58d87e92
Diego Gaspar <diegogaspardacruz1@gmail.com>
2021-01-17 03:26:31 -0300

    Add per method tests

--- src/services/__test__/rating.test.ts
+++ src/services/__test__/rating.test.ts
@@ -41,4 +41,84 @@ describe('Rating Service', () => {
       expect(rating).toBe(5);
     });
   });
+
+  /**
+   * Period calculation only tests
+   */
+  describe('Get rating based on swell period', () => {
+    it('Should get a rating of 1 for a period of 5 seconds', () => {
+      const rating = defaultRating.getRatingForSwellPeriod(5);
+      expect(rating).toBe(1);
+    });
+
+    it('Should get a rating of 2 for a period of 9 seconds', () => {
+      const rating = defaultRating.getRatingForSwellPeriod(9);
+      expect(rating).toBe(2);
+    });
+
+    it('Should get a rating of 4 for a period of 12 seconds', () => {
+      const rating = defaultRating.getRatingForSwellPeriod(12);
+      expect(rating).toBe(4);
+    });
+
+    it('Should get a rating of 5 for a period of 16 seconds', () => {
+      const rating = defaultRating.getRatingForSwellPeriod(16);
+      expect(rating).toBe(5);
+    });
+  });
+
+  /**
+   * Swell height specific logic calculation
+   */
+  describe('Get rating based on swell height', () => {
+    it('Should get a rating 1 of for less than ankle to knee high swell', () => {
+      const rating = defaultRating.getRatingForSwellSize(0.2);
+      expect(rating).toBe(1);
+    });
+
+    it('Should get a rating 2 of for less than ankle to knee high swell', () => {
+      const rating = defaultRating.getRatingForSwellSize(0.6);
+      expect(rating).toBe(2);
+    });
+
+    it('Should get a rating 3 of for less than ankle to knee high swell', () => {
+      const rating = defaultRating.getRatingForSwellSize(1.5);
+      expect(rating).toBe(3);
+    });
+
+    it('Should get a rating 5 of for less than ankle to knee high swell', () => {
+      const rating = defaultRating.getRatingForSwellSize(2.5);
+      expect(rating).toBe(5);
+    });
+  });
+
+  /**
+   * Location specific calculation
+   */
+  describe('Get position based on points location', () => {
+    it('Should get the point based on a east location', () => {
+      const response = defaultRating.getPositionFromLocation(92);
+      expect(response).toBe(BeachPosition.E);
+    });
+
+    it('Should get the point based on a north location 1', () => {
+      const response = defaultRating.getPositionFromLocation(360);
+      expect(response).toBe(BeachPosition.N);
+    });
+
+    it('Should get the point based on a north location 2', () => {
+      const response = defaultRating.getPositionFromLocation(40);
+      expect(response).toBe(BeachPosition.N);
+    });
+
+    it('Should get the point based on a south location', () => {
+      const response = defaultRating.getPositionFromLocation(200);
+      expect(response).toBe(BeachPosition.S);
+    });
+
+    it('Should get the point based on a wet location', () => {
+      const response = defaultRating.getPositionFromLocation(300);
+      expect(response).toBe(BeachPosition.W);
+    });
+  });
 });
