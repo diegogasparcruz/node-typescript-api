d344363af176e1d2f9950e01bc436f415e6055f0
Diego Gaspar <diegogaspardacruz1@gmail.com>
2021-01-18 09:47:18 -0300

    Update logger info message server port

--- src/server.ts
+++ src/server.ts
@@ -60,7 +60,7 @@ export class SetupServer extends Server {
 
   public start(): void {
     this.app.listen(this.port, () => {
-      logger.info('Server listening of port:', this.port);
+      logger.info(`Server listening of port: ${this.port}`);
     });
   }
 }
