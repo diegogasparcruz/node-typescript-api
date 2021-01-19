1a2a8e70b58761e63ba43c171a2312582df306b2
Diego Gaspar <diegogaspardacruz1@gmail.com>
2021-01-19 16:34:54 -0300

    Add api error validator middleware

--- src/server.ts
+++ src/server.ts
@@ -9,20 +9,25 @@ import * as database from '@src/database';
 import { BeachesController } from './controllers/beaches';
 import { UsersController } from './controllers/users';
 import logger from './logger';
-import apiSchema from './api.schema.json';
 import swaggerUi from 'swagger-ui-express';
 import { OpenApiValidator } from 'express-openapi-validator';
 import { OpenAPIV3 } from 'express-openapi-validator/dist/framework/types';
+import apiSchema from './api.schema.json';
+import { apiErrorValidator } from './middlewares/api-error-validator';
+import * as http from 'http';
 export class SetupServer extends Server {
+  private server?: http.Server;
+
   constructor(private port = 3000) {
     super();
   }
 
   public async init(): Promise<void> {
     this.setupExpress();
-    await this.docSetup();
+    await this.docsSetup();
     this.setupControllers();
     await this.databaseSetup();
+    this.setupErrorHandlers();
   }
 
   private setupExpress(): void {
@@ -39,6 +44,15 @@ export class SetupServer extends Server {
     );
   }
 
+  private async docsSetup(): Promise<void> {
+    this.app.use('/docs', swaggerUi.serve, swaggerUi.setup(apiSchema));
+    await new OpenApiValidator({
+      apiSpec: apiSchema as OpenAPIV3.Document,
+      validateRequests: true, //we do it
+      validateResponses: true,
+    }).install(this.app);
+  }
+
   private setupControllers(): void {
     const forecastController = new ForecastController();
     const beachesController = new BeachesController();
@@ -50,13 +64,8 @@ export class SetupServer extends Server {
     ]);
   }
 
-  private async docSetup(): Promise<void> {
-    this.app.use('/docs', swaggerUi.serve, swaggerUi.setup(apiSchema));
-    await new OpenApiValidator({
-      apiSpec: apiSchema as OpenAPIV3.Document,
-      validateRequests: true, //we do it
-      validateResponses: true,
-    });
+  private setupErrorHandlers(): void {
+    this.app.use(apiErrorValidator);
   }
 
   public getApp(): Application {
@@ -69,10 +78,18 @@ export class SetupServer extends Server {
 
   public async close(): Promise<void> {
     await database.close();
+    if (this.server) {
+      await new Promise((resolve, reject) => {
+        this.server?.close((err) => {
+          if (err) return reject(err);
+          resolve(true);
+        });
+      });
+    }
   }
 
   public start(): void {
-    this.app.listen(this.port, () => {
+    this.server = this.app.listen(this.port, () => {
       logger.info(`Server listening of port: ${this.port}`);
     });
   }
